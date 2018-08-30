6db0d59d0d60b0855c40204749d281de5ff43f04
wangkun <kunwang1@jieyuechina.com>
2018-08-30 20:05:30 +0800

    config centre auto-refresh

--- config-client/src/main/java/com/example/configclient/IndexController.java
+++ config-client/src/main/java/com/example/configclient/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @desc
  **/
 @RestController
+@RefreshScope
 public class IndexController {
     @Value("${ip}")
     private String ip;

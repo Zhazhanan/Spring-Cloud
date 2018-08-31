9e212c15d06b6a2a5084e6c6b2799a4671e86d1b
wangkun <kunwang1@jieyuechina.com>
2018-08-31 18:33:22 +0800

    zuul filter

--- zuul/src/main/java/com/example/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/example/zuul/ZuulApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Import(Filler.class)
 @EnableZuulProxy
 public class ZuulApplication {
 
